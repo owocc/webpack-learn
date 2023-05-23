d6f68c3e92d27f26fbad0bc2e0737a716a554a26
owocc <owocc@qq.com>
2023-05-23 23:31:28 +0800

    开发者选项(配置一堆开发者用的东西)

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
 const path = require("path");
 const HTMLWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
+  mode: "development",
   //配置入口文件
   entry: {
     index: "./src/index.js",
     print: "./src/print.js",
   },
+  devtool: "inline-source-map", //使用 source map
+  // 配置开发服务器
+  devServer: {
+    static: "./dist", //指定静态资源地址
+  },
   plugins: [
     new HTMLWebPackPlugin({
-      title: "管理输出", //配置输出的 index.html 的 title
+      title: "开发环境", //配置输出的 index.html 的 title
     }),
   ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"), //指定输出目录
     clean: true, //开启清理 dist 目录文件(就是将旧的文件删除)
+    publicPath: "/", //设置文件之间的引用路径
+  },
+  optimization: {
+    runtimeChunk: "single", // 控制webpack 是否将运行代码拆分成一个单独的 chunk
   },
 };
