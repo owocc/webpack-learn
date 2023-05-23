e33933bf080186335aadbe081a016c592d36cf46
owocc <owocc@qq.com>
2023-05-23 22:22:34 +0800

    管理输出

--- src/index.js
+++ src/index.js
@@ -1,30 +1,16 @@
 import _ from "lodash";
-import "./style.css"; // 导入 css 文件
-import agz from "./agz.png"; // 导入爱国者图片
-import Data from "./data/data.xml"; // 导入 xml 文件
-import Notes from "./data/data.csv"; // 导入 csv 文件数据
-import toml from "./data/data.toml"; // 导入 toml 文件
-import yaml from "./data/data.yaml"; //导入 yaml 文件
-import json5 from "./data/data.json5"; //导入 json5 文件
+import printMe from "./print";
 function component() {
   const element = document.createElement("div");
+  const btn = document.createElement("button"); //创建按钮
 
-  //lodash
   element.innerHTML = _.join(["hello", "webpack"], " ");
-  element.classList.add("hello"); //添加一个类名
 
-  //创建一个img标签,然后将它添加到已经创建了的 div 里
-  const agzImg = new Image();
-  agzImg.src = agz;
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;//给按钮添加点击事件
 
-  //将两个文件输出一下
-  console.log(Data); //输出导入的 xml 文件
-  console.log(Notes); //输出导入的 csv 文件
-  console.log(toml); //输出导入 toml 文件
-  console.log(yaml); //输出导入的 yaml 文件
-  console.log(json5); //输出导入的 json5 文件
+  element.appendChild(btn);
 
-  element.appendChild(agzImg); //将图片添加到div中
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,19 @@
 const path = require("path");
-const toml = require("toml");
-const yaml = require("yamljs");
-const json5 = require("json5");
+const HTMLWebPackPlugin = require("html-webpack-plugin");
 module.exports = {
   //配置入口文件
-  entry: "./src/index.js",
-  // 配置输出
-  output: {
-    filename: "bundle.js", //指定的文件名
-    path: path.resolve(__dirname, "dist"), //指定输出目录
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
   },
-
-  //添加模块,一些 loader (算是解析器吧,用于解析指定类型的文件)
-  module: {
-    rules: [
-      {
-        test: /\.css$/i, //指定要匹配的文件后缀(其实就是正则)
-        use: ["style-loader", "css-loader"], //配置 css loader
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i, //加载图片文件
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i, //配置字体文件 loader
-        type: "asset/resource",
-      },
-      {
-        test: /\.(csv|tsv)$/i, //添加 csv 和 tsv 格式的 loader
-        use: ["csv-loader"], //这里使用的 是 use,和之前的 css loader 一样,使用的是下载的包(大概是这个意思)
-      },
-      {
-        test: /\.xml$/i, // 添加 xml loader
-        use: ["xml-loader"],
-      },
-      // 使用自定义 parse 替换(增强)原始的 JSOn 解析器
-      {
-        // toml 文件解析
-        test: /\.toml$/i,
-        type: "json",
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        // yaml 文件解析
-        test: /\.yaml$/i,
-        type: "json",
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        // josn5 文件解析
-        test: /\.json5$/i,
-        type: "json",
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ],
+  plugins: [
+    new HTMLWebPackPlugin({
+      title: "管理输出", //配置输出的 index.html 的 title
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"), //指定输出目录
+    clean: true, //开启清理 dist 目录文件(就是将旧的文件删除)
   },
 };
